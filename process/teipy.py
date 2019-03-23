f58ed9d0b2a1702ef91cf65db58f245bab04a409
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-03-23 09:18:35 +0100

    some useful helper methods added

--- process/teipy.py
+++ process/teipy.py
@@ -9,6 +9,7 @@ class TeiReader():
     def __init__(self, xml):
         self.ns_tei = {'tei': "http://www.tei-c.org/ns/1.0"}
         self.ns_xml = {'xml': "http://www.w3.org/XML/1998/namespace"}
+        self.ns_tcf = {'tcf': "http://www.dspin.de/data/textcorpus"}
         self.nsmap = {
             'tei': "http://www.tei-c.org/ns/1.0",
             'xml': "http://www.w3.org/XML/1998/namespace",
@@ -18,18 +19,48 @@ class TeiReader():
         try:
             self.original = ET.parse(self.file)
         except Exception as e:
-            print(e)
-            self.original = ET.fromstring(self.file)
+            print('could not parse file')
+            try:
+                self.original = ET.fromstring(self.file.encode('utf8'))
+                print('read string worked')
+            except Exception as e:
+                print('read from string did not work')
+                print('try to download resource')
+                r = requests.get(self.file)
+                self.original = ET.fromstring(r.text)
         try:
             self.tree = ET.parse(self.file)
         except Exception as e:
-            print(e)
-            self.tree = ET.fromstring(self.file)
-        try:
-            self.parsed_file = ET.tostring(self.tree, encoding="utf-8")
+            print('could not parse tree')
+            try:
+                self.tree = ET.fromstring(self.file.encode('utf8'))
+                print('read string worked')
+            except Exception as e:
+                print('read from string did not work')
+                print('try to download resource')
+                r = requests.get(self.file)
+                self.tree = ET.fromstring(r.text)
         except Exception as e:
-            print(e)
+            print('parsing didnt work')
             self.parsed_file = "parsing didn't work"
+
+    def return_byte_like_object(self):
+        return ET.tostring(self.tree, encoding="utf-8")
+
+    def return_string(self):
+        return self.return_byte_like_object().decode('utf-8')
+
+    def tree_to_file(self, file=None):
+        """saves current tree to file"""
+        if file:
+            pass
+        else:
+            timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S')
+            file = "{}.xml".format(timestamp)
+
+        with open(file, 'wb') as f:
+            f.write(ET.tostring(self.tree))
+        return file
 
     def replace_child_nodes(self, xpath_to_parent, new_child_nodes):
         children_xpath = "{}/tei:*".format(xpath_to_parent)
